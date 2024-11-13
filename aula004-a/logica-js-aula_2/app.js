e79d6580f017b623839cf836573469e71b4d2ae9
Guilherme Alves Carchedi <guilhermealves.carchedi@gmail.com>
2024-11-13 15:00:07 -0300

    aula004-a do curso de JavaScript da alura

--- aula004-a/logica-js-aula_2/app.js
+++ aula004-a/logica-js-aula_2/app.js
@@ -1,15 +1,15 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt (Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentaivas = 1;
 
 // enquanto o chute não for igual o numero secreto
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentaivas} tentaivas`);
+        break
     } else {
         if(chute > numeroSecreto){
             alert (`O numero secreto é menor q ${chute}`);
@@ -20,3 +20,6 @@ while (chute != numeroSecreto){
         tentaivas++;
     }
 }
+
+let palavraTentativa = tentaivas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentaivas} ${palavraTentativa}`);
